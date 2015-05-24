48ae6da9314c959cf29d90cc79f9b9c57d5a2f71
Rein <MaxWang0@users.noreply.github.com>
2015-05-25 05:37:30 +0800

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,4 +1,5 @@
 public class Solution {
+       //recursive way
        public int Fib1(int n) {
               if( n == 1|n == 2 ) {
                 return 1;
@@ -6,6 +7,21 @@ public class Solution {
                 return Fib(n-1) + Fib(n-2);
                 }
               }
+       //unrecursive way
+       public int Fib2(int n) {
+              int nRet, nPp, nP = 1;
+              nRet, nPp, nP = 1;
+              
+              if( n == 1|n == 2 ) {
+                     return nRet;
+              for(int i = 3; i < n; i++ ){
+                     nRet = nP + nPp;
+                     nPp = nP;
+                     nP = nRet;
+              }
+              return nRet;
+              }
+       }
             }
           
        
