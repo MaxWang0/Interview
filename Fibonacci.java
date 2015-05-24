76f51c74cf48085fd4a19905c2039d7a5d1318f9
Rein <MaxWang0@users.noreply.github.com>
2015-05-25 05:11:58 +0800

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Solution {
+       public int Fib1(int n) {
+              if( n == 1|n == 2 ) {
+                return 1;
+              }else{
+                return Fib(n-1) + Fib(n-2);
+                }
+              }
+            }
+          
+       
+       
