7ef9e34db554da66de71f275a5b26a7a1d286a15
Rein <MaxWang0@users.noreply.github.com>
2015-06-06 01:58:57 +0800

    Update maxproduct2.java

--- maxproduct2.java
+++ maxproduct2.java
@@ -1,1 +1,21 @@
 public class solution{
+  public maxproduct2(int nums[]){
+    max_p  = 0;
+    start = 0;
+    end = 0;
+    
+    for(int i = 0; i < nums.length; i++){
+      sum_ = 0;
+      for(int j = i; j < nums.length; j++){
+        sum_ += nums[j];
+        if(sum_ > max_p){
+          max_p= sum_;
+          start = i;
+          end = j;
+        }
+      }
+    }
+    system.out.println(i + " " + j + " " + max_p);
+    return max_p;
+  }
+}
