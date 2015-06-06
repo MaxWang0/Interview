875bceb1f42352ce8bd1d1cc22ce84ff1da3a1c4
Rein <MaxWang0@users.noreply.github.com>
2015-06-06 08:31:05 +0800

    Update sortedArrayToBST.java

--- sortedArrayToBST.java
+++ sortedArrayToBST.java
@@ -1,1 +1,18 @@
 public class Solution{
+  public TreeNode sortedArrayToBST( int[] num){
+    if(num.length == 0) return null;
+    return rec(num, 0, num.length);
+  }
+  
+  public TreeNode rec(int[] num, int low, int high){
+    if(low == high) return null;
+    
+    int mid = (low + high)/2;
+    TreeNode root = new TreeNode(num[mid]);
+    
+    root.left = rec(num, low, mid);
+    root.right = rec(num, mid, right);
+    
+    return root;
+  }
+}
