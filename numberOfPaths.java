d1ce425b27608c07bfb8e5bf75a2f184f13613ca
Rein <MaxWang0@users.noreply.github.com>
2015-06-07 02:17:20 +0800

    Create numberOfPaths.java

---
+++ numberOfPaths.java
@@ -0,0 +1,1 @@
+//Consider matrix/maze of nxm robot at one corner needs to move to diagonally opposite cell.The robot can move only top or left.Write code that returns number of ways he can reach the target cell.
