b95ba101729938246a38cf4bba863b8a4cae8057
Rein <MaxWang0@users.noreply.github.com>
2015-06-06 23:35:15 +0800

    Update reversearray.java

--- reversearray.java
+++ reversearray.java
@@ -1,1 +1,9 @@
 public class Solution{
+  public reversearray(int[] nums){
+    for(int i = 0; i < (nums.length-1)/2; i++){
+      nums[i] = nums[i] + nums[nums.length - i -1];
+      nums[nums.length - i - 1] = nums[i] - nums[nums.length - i -1];
+      nums[i] = nums[i] - nums[nums.length - i - 1];
+    }
+  }
+}
