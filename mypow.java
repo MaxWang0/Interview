00a9ac2a4167edac034030bf574c8bbbc7fa216b
Rein <MaxWang0@users.noreply.github.com>
2015-06-06 08:10:37 +0800

    Update mypow.java

--- mypow.java
+++ mypow.java
@@ -1,1 +1,23 @@
+public class Solution {
+    public double Pow(double x, int n) {
+	if (n == 0)
+		return 1;
+ 
+	double v = Pow(x, n / 2);
+ 
+	if (n % 2 == 0) {
+		return v * v;
+	} else {
+		return v * v * x;
+	}
+}
+ 
+    public double myPow(double x, int n) {
+	if (n < 0) {
+		return 1 / Pow(x, -n);
+	} else {
+		return Pow(x, n);
+	}
+}
 
+    }
