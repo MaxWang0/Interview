df0b830cab92766273548b981b0237782a91b8d0
Rein <MaxWang0@users.noreply.github.com>
2015-06-08 00:19:33 +0800

    Update reverseWords.java

--- reverseWords.java
+++ reverseWords.java
@@ -1,1 +1,16 @@
 Public class Solution{
+  public String reverseWords(String words){
+    
+    if (words.length() == 0 || words == null) return null;
+    
+    newstr = words.split("\\s+"); 
+    StringBuffer str = new StringBuffer();
+    
+    for(int i = newstr.length-1; i >= 0, i--){
+      str.append(newstr[i])
+      str.append(" ");
+    }
+    
+    return str.toString().trim();
+  }
+}
