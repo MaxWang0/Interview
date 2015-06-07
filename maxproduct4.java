b12a4b1b5fd947e026434484d388c2a9b66249f6
Rein <MaxWang0@users.noreply.github.com>
2015-06-07 10:38:22 +0800

    Update maxproduct4.java

--- maxproduct4.java
+++ maxproduct4.java
@@ -1,6 +1,14 @@
 public class Solution{
   public int maxSubArray(int[] A){
     int max = A[0];
+    int sum_ = A[0];
+    
+    for(int i = 1; i < A.length; i++){
+      sum_ = Math.max(A[i], A[i] + sum_ );
+      max = Math.max(max, sum_);
+    }
+    
+    return max;
     
   }
 }
