5143f0798c6ea6511e653b812f793541d77ce64f
Rein <MaxWang0@users.noreply.github.com>
2015-06-08 10:20:56 +0800

    Update Fibonacci.pl

--- Fibonacci.pl
+++ Fibonacci.pl
@@ -4,4 +4,14 @@ use strict;
 use warnings;
 
 sub fib{
+  my $n = shift;
+  
+  if($n == 1 or $n == 2){
+    return 1;
+  }else{
+    return fib($n-1) + fib($n-2);
+  }
+}
+
+print "fib($nums)\n";
   
