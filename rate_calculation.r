c944d52433465e90d8c19b7d64f5a1de0b3459ad
Rein <MaxWang0@users.noreply.github.com>
2015-06-21 04:33:13 +0800

    Update rate_calculation.r

--- rate_calculation.r
+++ rate_calculation.r
@@ -1,1 +1,10 @@
+quote <- read.table("output_quotes.csv", header = T, sep = "\t")
+invite <- read.table("output_invites.csv", header = T, sep = "\t")
+rate <- round(quote[,2]/invite[,2], 2)   
+output_rate <- cbind(as.character(quote[,1]), rate, quote[,2], invite[,2])
+title <-c("sent_time", "rate", "quote", "invite")
+output_rate <- rbind(title, output_rate)
+write.table(output_rate, "output_rate.csv", row.names = F, col.names = F, sep = "\t", quote = F)
+rate <- read.table("output_rate.csv", header = T, sep = "\t")
+plot(rate[,1], rate[,2], main = "rate_timeseries", xlab = "time_sent", ylab = "rate" , ylim = c(0,1.5))
 
