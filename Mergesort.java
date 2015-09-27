b78190bda9bb4fef1e53a07c1b73e26f0f2816b3
Rein <MaxWang0@users.noreply.github.com>
2015-09-28 03:35:52 +0800

    Update Mergesort.java

--- Mergesort.java
+++ Mergesort.java
@@ -1,10 +1,49 @@
 public class Solution{
   public mergesort(A[], start, end){
+    
     int mid = (start+end)/2;
     mergesort(A[], 0, mid);
     mergesort(A[], mid+1, end);
     merge(A[], mergesort(A[], 0, mid), mergesort(A[], mid+1, end));
   }
   
-  merge()
+  merge(A[], B[], C[]){
+    b = B.length;
+    c = C.length;
+    if(b == 0| c != 0){
+      for(int i = 0; i < c;  i++){
+        A[i] = C[i];
+      }
+    }else if(c == 0| b != 0){
+      for(int i = 0; i < c;  i++){
+        A[i] = B[i];
+      }
+    }else{
+      return;
+    }
+    int n = 0;
+    int i = 0;
+    int j = 0;
+    while(n < b+c){
+      if(i > b){
+        A[n] = C[j];
+        j++;
+        n++;
+      }
+      if( j > c){
+        A[n] = B[i];
+        i++;
+        n++;
+      }
+      if(B[i] > C[j]){
+        A[n] = C[j];
+        j++;
+        n++;
+      }else if(B[i] < C[j]){
+        A[n] = B[i];
+        i++;
+        n++;
+      }
+    }
+  }
 }
