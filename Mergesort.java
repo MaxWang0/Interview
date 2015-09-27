90c101504512e6cee4ed5a5b2094fe5bf71fd2e4
Rein <MaxWang0@users.noreply.github.com>
2015-09-28 03:14:24 +0800

    Update Mergesort.java

--- Mergesort.java
+++ Mergesort.java
@@ -1,1 +1,10 @@
-
+public class Solution{
+  public mergesort(A[], start, end){
+    int mid = (start+end)/2;
+    mergesort(A[], 0, mid);
+    mergesort(A[], mid+1, end);
+    merge(A[], mergesort(A[], 0, mid), mergesort(A[], mid+1, end));
+  }
+  
+  merge()
+}
