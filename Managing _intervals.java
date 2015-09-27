b668c265449a31e6460b20955d771962a0fce635
Rein <MaxWang0@users.noreply.github.com>
2015-09-28 04:18:58 +0800

    Update Managing _intervals.java

--- Managing _intervals.java
+++ Managing _intervals.java
@@ -1,1 +1,12 @@
-
+public class Solution{
+  public interval(input[][], output[]){
+    n = input.length;
+    m = 0
+    for(int i = 0; i < n-2; i++){
+      if(input[i][2] > input[i+1][1] && input[i][1] < input[i+1][2]){
+        output[m++] = input[i][1];
+        output[m++] = input[i+1][2];
+      }
+    }
+  }
+}
