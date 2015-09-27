547b13d76a59f1ea8dd40c47e55c3828fb9fa1e6
Rein <MaxWang0@users.noreply.github.com>
2015-09-28 03:06:19 +0800

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,7 +1,7 @@
 def array_pair(arr, target):
   if len(ar) = 0 or len(arr) = 1
     return None
-  for i in arr    (O(n))
+  for i in arr               #(O(n))
     paired = tareget - i
     if parired in arr        #(O(1))
       return (paired, i)
