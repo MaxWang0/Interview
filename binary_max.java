3e662e6675f81aff8349267f01bc5e9ec9abcb18
Rein <MaxWang0@users.noreply.github.com>
2016-09-22 00:34:06 -0500

    Create binary_max.java
    Given a list of numbers, a[0], a[1], a[2], … , a[N-1], where 0 <= a[i] < 2^32, find the maximum result of a[i] XOR a[j]

---
+++ binary_max.java
@@ -0,0 +1,18 @@
+class Solution{
+   class TreeNode{
+         TreeNode[] node;
+         public TreeNode(){
+            node = new TreeNode[2];
+         }
+         
+         public insert(String str, int n){
+             
+         }
+   }
+   
+   public int binary_max(int[] nums){
+       for(int i = 0; i < nums.length; i++){
+           
+       }
+   }
+}
