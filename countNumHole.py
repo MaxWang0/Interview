dd49add3ebc8f0d715cc4c74c8c0fb29c6013b20
Rein <MaxWang0@users.noreply.github.com>
2016-10-11 12:05:22 -0500

    Update countNumHole.py

--- countNumHole.py
+++ countNumHole.py
@@ -1,1 +1,24 @@
-
+def countHole(num):
+	hash= {}
+	hash[0]=1
+	hash[1]=0
+	hash[2]=0
+	hash[3]=0
+	hash[4]=1
+	hash[5]=0
+	hash[6]=1
+	hash[7]=1
+	hash[8]=2
+	hash[9]=1
+	rem = 0
+	div = 0;
+	sum =0
+	while(num>0):
+		rem=num%10
+		num = int(num/10)
+		sum+=hash[rem]
+	return sum	
+def main():
+	print(countHole(123456))
+	
+main()
